32c6f978aa6b046ae394590262de1b5b202de2f9
Joyceeee <joyce_jonker2@hotmail.com>
2018-03-09 09:55:49 +0100

    Test weghalen

--- app/src/main/java/nl/joycenienkeeveline/connectfour/tudelft/ide/software/appie/MainActivity.java
+++ app/src/main/java/nl/joycenienkeeveline/connectfour/tudelft/ide/software/appie/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
 
-    lalalalla teste testsete
+    lalalall
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
