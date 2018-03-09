df734d3bee02ea17e85e02c239c4c876a1aed954
Joyceeee <joyce_jonker2@hotmail.com>
2018-03-09 10:24:06 +0100

    dgdg

--- app/src/main/java/nl/joycenienkeeveline/connectfour/tudelft/ide/software/appie/MainActivity.java
+++ app/src/main/java/nl/joycenienkeeveline/connectfour/tudelft/ide/software/appie/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
 
-    lalalall hsjkfhskjdfjuyuyuiy sldfjlsdjflskd
+    lalalall hsjkfhskjdfjuyuyuiy sldfjlsdjflskd 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
