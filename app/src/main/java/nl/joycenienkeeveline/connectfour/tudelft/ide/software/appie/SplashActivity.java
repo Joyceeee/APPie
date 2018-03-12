6c910604102fe3384c34bba006672b61b71aeb54
EvelinePientje <evelinedrijver@gmail.com>
2018-03-12 12:40:45 +0100

    Loadscreen 2.2

---
+++ app/src/main/java/nl/joycenienkeeveline/connectfour/tudelft/ide/software/appie/SplashActivity.java
@@ -0,0 +1,28 @@
+package nl.joycenienkeeveline.connectfour.tudelft.ide.software.appie;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by evelinepientje on 09-03-18.hoi
+ */
+
+
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainMenu.class);
+        startActivity(intent);
+        finish(); 
+    }
+}
+
+
+
+    //https://www.bignerdranch.com/blog/splash-screens-the-right-way/
